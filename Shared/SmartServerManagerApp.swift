1b0f5a2c406ffec7a488c1275d83b2f48b73a40b
pascal <pepe44y@gmail.com>
2021-08-21 20:04:55 +0200

    Initial Commit

---
+++ Shared/SmartServerManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartServerManagerApp.swift
+//  Shared
+//
+//  Created by Pascal Priessnitz on 21.08.21.
+//
+
+import SwiftUI
+
+@main
+struct SmartServerManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
